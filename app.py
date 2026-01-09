55536c3b98273afa3203e054ab0b6394fc6479cf
unknown <kwame.boahene@live.com>
2026-01-09 10:42:39 +0000

    Initial python web app

---
+++ app.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class AppHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"App is running")
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8000), AppHandler)
+    server.serve_forever()
+
